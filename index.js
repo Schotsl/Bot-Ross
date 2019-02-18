091acc17f16317c2f00f0eb77df307f0476e09a3
Sjors van Holst <sjorsvanholst@gmail.com>
2019-02-19 00:05:33 +0100

    Added notice function

--- index.js
+++ index.js
@@ -38,72 +38,84 @@ function party() {
   });
 }
 
-function getRandomInteger(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
+function notice() {
+  let oldStatus = new Array();
+  officeLights.forEach(function(officeLight) {
+    oldStatus.push(officeLigh.getState());
+  })
 
+  //Actually do stuff
 
-function permissionlookup(permission, message) {
-  if (message.member.id == "127076783714598912") return true;
-  if (!message.member.permissions.has([permission], true)) {
-    message.channel.send(`<@${message.author.id}> You dont have permissions to do that! You need **${permission.toLowerCase()}**.`);
-    return false;
+  officeLights.forEach(function(officeLight, index) {
+    officeLight.setState(oldStatus[index]);
+  })}
+
+  function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
   }
-}
-
-bot.on("ready", async () => {
-  report.log(`Bot is ready. ${bot.user.username}`);
-
-  try {
-    let link = await bot.generateInvite(["ADMINISTRATOR"]);
-    report.log(link);
-  } catch(e) {
-    report.log(e.stack);
-  }
-});
-
-bot.on("message", async message => {
-  if(message.author.bot) return;
-  if((message.channel.type) === "dm") return;
-
-  if(!message.content.startsWith(botoptions.prefix)) return;
-
-  let messageArray = message.content.split(" ");
-  let command = messageArray[0];
-
-  command = command.substring(1);
-
-  switch (command.toLowerCase()) {
-    case "info":
-    message.channel.send(botoptions.info);
-    break;
-
-    case "party":
-    party();
-    break;
-
-    case "toggle":
-    if (talkedRecently.has(msg.author.id)) {
-      msg.channel.send("Wait 1 minute before getting typing this again. - " + msg.author);
-    } else {
-
-      toggle();
-      message.channel.send("Toggled the lights.");
 
 
-      talkedRecently.add(msg.author.id);
-      setTimeout(() => {
-        // Removes the user from the set after a minute
-        talkedRecently.delete(msg.author.id);
-      }, 60000);
+  function permissionlookup(permission, message) {
+    if (message.member.id == "127076783714598912") return true;
+    if (!message.member.permissions.has([permission], true)) {
+      message.channel.send(`<@${message.author.id}> You dont have permissions to do that! You need **${permission.toLowerCase()}**.`);
+      return false;
     }
-
-    break;
-
-    default:
-    message.channel.send(botoptions.default);
-    break;
   }
-})
 
-bot.login(botoptions.token);
+  bot.on("ready", async () => {
+    report.log(`Bot is ready. ${bot.user.username}`);
+
+    try {
+      let link = await bot.generateInvite(["ADMINISTRATOR"]);
+      report.log(link);
+    } catch(e) {
+      report.log(e.stack);
+    }
+  });
+
+  bot.on("message", async message => {
+    if(message.author.bot) return;
+    if((message.channel.type) === "dm") return;
+
+    if(!message.content.startsWith(botoptions.prefix)) return;
+
+    let messageArray = message.content.split(" ");
+    let command = messageArray[0];
+
+    command = command.substring(1);
+
+    switch (command.toLowerCase()) {
+      case "info":
+      message.channel.send(botoptions.info);
+      break;
+
+      case "party":
+      party();
+      break;
+
+      case "toggle":
+      if (talkedRecently.has(msg.author.id)) {
+        msg.channel.send("Wait 1 minute before getting typing this again. - " + msg.author);
+      } else {
+
+        toggle();
+        message.channel.send("Toggled the lights.");
+
+
+        talkedRecently.add(msg.author.id);
+        setTimeout(() => {
+          // Removes the user from the set after a minute
+          talkedRecently.delete(msg.author.id);
+        }, 60000);
+      }
+
+      break;
+
+      default:
+      message.channel.send(botoptions.default);
+      break;
+    }
+  })
+
+  bot.login(botoptions.token);
