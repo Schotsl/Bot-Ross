627de25f9e7c0af66684ff26b4f967f463b9d487
ElectroGamez <wouter2001.wdb@gmail.com>
2019-02-19 02:43:04 +0100

    Added a error object in settings.json. Updates temp.

--- index.js
+++ index.js
@@ -115,15 +115,27 @@ bot.on("message", async message => {
       break;
 
       case "temp":
+      let newColorTemp = LightState.create();
       if (isNaN(messageArray[1])) {
-        let newColorTemp = LightState.create();
-
         if (messageArray[1] == "warm") officeGroups[0].setGroup(newColorTemp.ct(400));
         else if (messageArray[1] == "ice") officeGroups[0].setGroup(newColorTemp.ct(153));
         else if (messageArray[1] == "hot") officeGroups[0].setGroup(newColorTemp.ct(500));
         else if (messageArray[1] == "cold") officeGroups[0].setGroup(newColorTemp.ct(253));
+        else return message.channel.send(settings.errors.temp);
+      } else {
+        if (messageArray[1] > 500 || messageArray[1] < 0) return message.channel.send(settings.errors.temp);
+        officeGroups[0].setGroup(newColorTemp.ct(messageArray[1]));
       }
-      message.channel.send("Changing color temp...");
+      break;
+
+      case "bri":
+      if (isNaN(messageArray[1])) {
+        let newColorTemp = LightState.create();
+
+        if (messageArray[1] == "min") officeGroups[0].setGroup(newColorTemp.bri(50));
+        else if (messageArray[1] == "max") officeGroups[0].setGroup(newColorTemp.bri(255));
+      }
+      message.channel.send("test");
       break;
 
       default:
