8938d45e60ce1bfb9905a0f7a5df4af2059adfeb
ElectroGamez <wouter2001.wdb@gmail.com>
2019-02-19 02:46:16 +0100

    fixed timeout not reading from settings

--- index.js
+++ index.js
@@ -145,7 +145,7 @@ bot.on("message", async message => {
     talkedRecently.add(message.author.id);
     setTimeout(() => {
       talkedRecently.delete(message.author.id);
-    }, 60000);
+    }, settings.timeout);
   }
 })
 
