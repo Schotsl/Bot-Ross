f2e89e0c48293389998b01e37216f019287ba637
Sjors van Holst <sjorsvanholst@gmail.com>
2019-02-19 01:49:32 +0100

    Indentation

--- index.js
+++ index.js
@@ -65,64 +65,64 @@ bot.on("ready", async () => {
 });
 
 bot.on("message", async message => {
-if (message.content.startsWith(botoptions.prefix)) {
+  if (message.content.startsWith(botoptions.prefix)) {
 
-  let messageArray = message.content.split(" ");
-  let command = messageArray[0];
+    let messageArray = message.content.split(" ");
+    let command = messageArray[0];
 
-  command = command.substring(1);
+    command = command.substring(1);
 
-  switch (command.toLowerCase()) {
-    case "info":
-    message.channel.send(botoptions.info);
-    break;
+    switch (command.toLowerCase()) {
+      case "info":
+      message.channel.send(botoptions.info);
+      break;
 
-    case "party":
-    message.channel.send("Get the party started");
-    party();
-    break;
+      case "party":
+      message.channel.send("Get the party started");
+      party();
+      break;
 
-    case "toggle":
-    if (talkedRecently.has(message.author.id)) {
-      message.channel.send("Wait 1 minute before getting typing this again. - " + message.author);
-    } else {
+      case "toggle":
+      if (talkedRecently.has(message.author.id)) {
+        message.channel.send("Wait 1 minute before getting typing this again. - " + message.author);
+      } else {
 
-      toggle();
-      message.channel.send("Toggled the lights.");
+        toggle();
+        message.channel.send("Toggled the lights.");
 
-      talkedRecently.add(message.author.id);
-      setTimeout(() => {
-        // Removes the user from the set after a minute
-        talkedRecently.delete(message.author.id);
-      }, 60000);
-    }
-    break;
+        talkedRecently.add(message.author.id);
+        setTimeout(() => {
+          // Removes the user from the set after a minute
+          talkedRecently.delete(message.author.id);
+        }, 60000);
+      }
+      break;
 
-    case "set":
-    if (isNaN(messageArray[1] && isNaN(messageArray[2]))) return message.channel.send("Please provide a number and off/on");
-    if (!(messageArray[2] == "off" || messageArray[2] == "on")) return message.channel.send("Please provide a number and off/on");
-    let state = messageArray[2] == "off" ? false : true;
+      case "set":
+      if (isNaN(messageArray[1] && isNaN(messageArray[2]))) return message.channel.send("Please provide a number and off/on");
+      if (!(messageArray[2] == "off" || messageArray[2] == "on")) return message.channel.send("Please provide a number and off/on");
+      let state = messageArray[2] == "off" ? false : true;
 
-    let stateObject = {
-      on: state,
-    }
-    officeLights[messageArray[1] -1].setState(stateObject);
-    break;
+      let stateObject = {
+        on: state,
+      }
+      officeLights[messageArray[1] -1].setState(stateObject);
+      break;
 
-    case "temp":
-    if (isNaN(messageArray[1])) {
-      if (messageArray[1] == "warm") return groupTemp(400, officeGroups[0]);
-      if (messageArray[1] == "cold") return groupTemp(253, officeGroups[0]);
-      if (messageArray[1] == "ice") return groupTemp(153, officeGroups[0]);
-      if (messageArray[1] == "hot") return groupTemp(500, officeGroups[0]);
-    }
-    message.channel.send("Changing color temp...");
-    break;
+      case "temp":
+      if (isNaN(messageArray[1])) {
+        if (messageArray[1] == "warm") return groupTemp(400, officeGroups[0]);
+        if (messageArray[1] == "cold") return groupTemp(253, officeGroups[0]);
+        if (messageArray[1] == "ice") return groupTemp(153, officeGroups[0]);
+        if (messageArray[1] == "hot") return groupTemp(500, officeGroups[0]);
+      }
+      message.channel.send("Changing color temp...");
+      break;
 
-    default:
+      default:
       message.channel.send(botoptions.default);
-    break;
-  }
+      break;
+    }
   }
 })
 
