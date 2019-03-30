bff2a3ea1f813b9021a8274899590a81d153895b
Sjors van Holst <sjorsvanholst@gmail.com>
2019-03-30 20:13:03 +0100

    Added party command

---
+++ commands/party.js
@@ -0,0 +1,31 @@
+module.exports = class Party extends Command {
+  constructor() {
+    super();
+    this.trigger = "party";
+  }
+
+  execute(input) {
+    // message.channel.send(language.respond('confirm', emotion));
+    // report.log(`"${message.author}" used the ".party" command`);
+
+    lampArray.forEach(function(officeLight) {
+      officeLight.getState(function(startState) {
+
+        let totalOffset = 0;
+        for (let i = 0; i < 10; i ++ ) {
+          totalOffset += functions.getRandomInteger(100, 1000);
+          setTimeout(function() {
+            let newLightState = LightState.create().on();
+            newLightState.ct(functions.getRandomInteger(153, 500));
+            newLightState.bri(functions.getRandomInteger(0, 255));
+            newLightState.transitionInstant()
+
+            //If last callback, return the lights to normal
+            if (i == 9) officeLight.setState(startState);
+            else officeLight.setState(newLightState);
+          }, totalOffset);
+        }
+      });
+    });
+  }
+}
