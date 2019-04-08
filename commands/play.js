8e689d567ba1a6dbc5ea2fe5a0b2172b4a98c160
Sjors van Holst <sjorsvanholst@gmail.com>
2019-04-08 17:12:53 +0200

    Added .play command

---
+++ commands/play.js
@@ -0,0 +1,19 @@
+const ytdl = require('ytdl-core');
+
+module.exports = class Play extends Command {
+  constructor() {
+    super();
+    this.trigger = "play";
+  }
+
+  async executeCustom(command, input, message) {
+    let channelId = message.member.voiceChannelID;
+    let channelObject = bot.channels.get(channelId);
+
+    let streamObject = channelObject.join().then((connection) => {
+      const streamOptions = { seek: 0, volume: 1 };
+      const stream = ytdl('https://www.youtube.com/watch?v=Zkw_jSHtXvg', { filter : 'audioonly' });
+      const dispatcher = connection.playStream(stream, streamOptions);
+    })
+  }
+}
