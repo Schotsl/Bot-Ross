edceeeb6e0c4ab60d8169402d88e08743aed1df1
Sjors van Holst <sjorsvanholst@gmail.com>
2019-04-09 13:33:22 +0200

    Added Queue functionality

--- commands/play.js
+++ commands/play.js
@@ -4,16 +4,38 @@ module.exports = class Play extends Command {
   constructor() {
     super();
     this.trigger = "play";
+
+    this.playing = false;
+    this.queue = new Array();
   }
 
   async executeCustom(command, input, message) {
     let channelId = message.member.voiceChannelID;
     let channelObject = bot.channels.get(channelId);
 
-    global.voiceChannelObject = await channelObject.join();
-
     const stream = ytdl(input[0], { filter : 'audioonly' });
-    const dispatcher = voiceChannelObject.playStream(stream);
-    dispatcher.on("end", end => voiceChannelObject.disconnect());
+    this.queue.push(stream);
+
+    if (!this.playing) {
+      global.voiceChannelObject = await channelObject.join();
+      this.playing = true;
+      this.playQueue();
+    }
+  }
+
+  playQueue() {
+    if (this.queue.length > 0) {
+      let streamObject = this.queue[0];
+      const dispatcher = voiceChannelObject.playStream(streamObject);
+
+      let that = this;
+      dispatcher.on("end", end => {
+        that.queue.shift();
+        that.playQueue();
+      });
+    } else {
+      voiceChannelObject.disconnect()
+      this.playing = false;
+    }
   }
 }
