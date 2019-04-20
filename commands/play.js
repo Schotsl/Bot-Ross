dc76d4ab37a69bacd2b07fb130d4e104a6fe26ab
Sjors van Holst <sjorsvanholst@gmail.com>
2019-04-20 23:43:26 +0200

    Fixed memory leak

--- commands/play.js
+++ commands/play.js
@@ -13,7 +13,7 @@ module.exports = class Play extends Command {
     let channelId = message.member.voiceChannelID;
     let channelObject = bot.channels.get(channelId);
 
-    const stream = ytdl(input[0], { filter : 'audioonly' });
+    let stream = ytdl(input[0], { filter : 'audioonly' });
     this.queue.push(stream);
 
     if (!this.playing) {
