999b2508e4aaed77a3c61edb3f7e54a54f6c0073
Sjors van Holst <sjorsvanholst@gmail.com>
2019-05-29 17:08:47 +0200

    Started command refacotring

---
+++ commands/lights.js
@@ -0,0 +1,49 @@
+let lightState = require('node-hue-api').lightState;
+
+module.exports = class Lights extends Command {
+  constructor() {
+    super();
+    this.commands = [
+      {trigger: "off", function: "off", description: "Turn off the lights", timeout: 1000, executed: {}},
+      {trigger: "on", function: "on", description: "Turn on the lights", timeout: 1000, executed: {}}
+    ];
+  }
+
+  off(input, message) {
+    sentenceRepository.getClosestIntention('confirm', emotionValue, (sentenceCollection) => {
+      message.channel.send(sentenceCollection.getSentences()[0].getContent());
+    });
+
+    sentenceRepository.getClosestIntention('off', emotionValue, (sentenceCollection) => {
+      message.channel.send(sentenceCollection.getSentences()[0].getContent());
+    });
+
+    let newLightState = lightState.create();
+    lightRepository.getAll((lightCollection) => {
+      let lightArray = lightCollection.getLights();
+
+      lightArray.forEach((lightSingle) => {
+        lightSingle.setState(newLightState.off());
+      });
+    });
+  }
+
+  on(input, message) {
+    sentenceRepository.getClosestIntention('confirm', emotionValue, (sentenceCollection) => {
+      message.channel.send(sentenceCollection.getSentences()[0].getContent());
+    });
+
+    sentenceRepository.getClosestIntention('on', emotionValue, (sentenceCollection) => {
+      message.channel.send(sentenceCollection.getSentences()[0].getContent());
+    });
+
+    let newLightState = lightState.create();
+    lightRepository.getAll((lightCollection) => {
+      let lightArray = lightCollection.getLights();
+
+      lightArray.forEach((lightSingle) => {
+        lightSingle.setState(newLightState.on());
+      });
+    });
+  }
+}
