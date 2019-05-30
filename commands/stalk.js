c5f354e59125ce4ef72552fd3e35c354912a7db0
Sjors van Holst <sjorsvanholst@gmail.com>
2019-05-30 19:49:59 +0200

    Fixed notify bug

--- commands/stalk.js
+++ commands/stalk.js
@@ -15,34 +15,41 @@ module.exports = class Stalk extends Command {
       let personObject = personCollection.getPersons()[0];
 
       bot.on("voiceStateUpdate", function(oldMember, newMember) {
-        //If voice channel change
-        if (oldMember.voiceChannelID !== newMember.voiceChannelID) {
-          let channelObjects = newMember.guild.channels;
-          if (newMember.voiceChannelID !== null && oldMember.voiceChannelID !== null) message.channel.send(`${personObject.getFullname()} changed to ${channelObjects.get(newMember.voiceChannelID).name}`);
-          else if (oldMember.voiceChannelID === null) message.channel.send(`${personObject.getFullname()} joined ${channelObjects.get(newMember.voiceChannelID).name}`);
-          else if (newMember.voiceChannelID === null) message.channel.send(`${personObject.getFullname()} left ${channelObjects.get(oldMember.voiceChannelID).name}`);
-        }
+        if (oldMember.id === personObject.getDiscord() || newMember.id === personObject.getDiscord()) {
 
-        //If voice mute change
-        if (oldMember.selfMute !== newMember.selfMute) {
-          if (newMember.selfMute) message.channel.send(`${personObject.getFullname()} muted itself`);
-          else message.channel.send(`${personObject.getFullname()} unmuted its self`);
-        }
+          //If voice channel change
+          if (oldMember.voiceChannelID !== newMember.voiceChannelID) {
+            let channelObjects = newMember.guild.channels;
+            if (newMember.voiceChannelID !== null && oldMember.voiceChannelID !== null) message.channel.send(`${personObject.getFullname()} changed to ${channelObjects.get(newMember.voiceChannelID).name}`);
+            else if (oldMember.voiceChannelID === null) message.channel.send(`${personObject.getFullname()} joined ${channelObjects.get(newMember.voiceChannelID).name}`);
+            else if (newMember.voiceChannelID === null) message.channel.send(`${personObject.getFullname()} left ${channelObjects.get(oldMember.voiceChannelID).name}`);
+          }
 
-        //If voice mute change
-        if (oldMember.selfDeaf !== newMember.selfDeaf) {
-          if (newMember.selfDeaf) message.channel.send(`${personObject.getFullname()} deafened itself`);
-          else message.channel.send(`${personObject.getFullname()} undeafened itself`);
+          //If voice mute change
+          if (oldMember.selfMute !== newMember.selfMute) {
+            if (newMember.selfMute) message.channel.send(`${personObject.getFullname()} muted itself`);
+            else message.channel.send(`${personObject.getFullname()} unmuted its self`);
+          }
+
+          //If voice mute change
+          if (oldMember.selfDeaf !== newMember.selfDeaf) {
+            if (newMember.selfDeaf) message.channel.send(`${personObject.getFullname()} deafened itself`);
+            else message.channel.send(`${personObject.getFullname()} undeafened itself`);
+          }
+
         }
       });
 
       bot.on("presenceUpdate", function(oldMember, newMember) {
-        //If status change
-        if (oldMember.selfDeaf !== newMember.selfDeaf) {
-          message.channel.send(`${personObject.getFullname()} status changed to ${newMember.presence.status}`);
+        if (oldMember.id === personObject.getDiscord() || newMember.id === personObject.getDiscord()) {
+
+          //If status change
+          if (oldMember.selfDeaf !== newMember.selfDeaf) {
+            message.channel.send(`${personObject.getFullname()} status changed to ${newMember.presence.status}`);
+          }
+          
         }
       });
     })
   }
-
 }
