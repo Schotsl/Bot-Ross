56d2148e1e14e1c33f4e533f33486c5107444d7b
Sjors van Holst <sjorsvanholst@gmail.com>
2019-06-17 20:21:36 +0200

    Added fatal function to report

--- classes/report.js
+++ classes/report.js
@@ -43,6 +43,26 @@ module.exports = class Report {
     console.error(`${currentTimeString} [ERROR]: ${rawLine}`);
   }
 
+  fatal(rawLine) {
+    let currentDateObject = new Date();
+
+    let currentDateDay = `0${currentDateObject.getDate()}`.slice(-2);
+    let currentDateMonth = `0${currentDateObject.getMonth()}`.slice(-2);
+    let currentDateYear = `0${currentDateObject.getFullYear()}`.slice(-2);
+    let currentDateString = `${currentDateDay}-${currentDateMonth}-${currentDateYear}`;
+
+    let currentTimeHour = `0${currentDateObject.getHours()}`.slice(-2);
+    let currentTimeMinute = `0${currentDateObject.getMinutes()}`.slice(-2);
+    let currentTimeSecond = `0${currentDateObject.getSeconds()}`.slice(-2);
+    let currentTimeMillis = `000${currentDateObject.getMilliseconds()}`.slice(-3);
+    let currentTimeString = `${currentTimeHour}:${currentTimeMinute}:${currentTimeSecond}.${currentTimeMillis}`;
+
+    this.write(`${currentTimeString} [FATAL]: ${rawLine}\n`, `${currentDateString}.txt`, `logs`);
+    console.error(`${currentTimeString} [FATAL]: ${rawLine}`);
+
+    process.exit();
+  }
+
   write(logFileEntry, logFileName, logFileDirectory) {
     if (!fs.existsSync(logFileDirectory)) fs.mkdirSync(logFileDirectory);
     if (!fs.existsSync(`${logFileDirectory}/${logFileName}`)) fs.openSync(`${logFileDirectory}/${logFileName}`, `w`);
