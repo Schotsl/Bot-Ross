7cdae71fd23ff8c82c0230cbf0904083324a7ab8
Sjors van Holst <sjorsvanholst@gmail.com>
2019-06-17 20:49:27 +0200

    Added loader

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 "use strict";
 
 //Non constructors
-global.fs = require(`fs`);
 global.functions = require(`./functions.js`);
+global.loader = require(`./loader.js`);
 
 //Custom classes
 global.Events = require('events');
@@ -10,32 +10,14 @@ global.Report = require(`./classes/report.js`);
 global.Command = require(`./classes/command.js`);
 global.Protocol = require(`./classes/protocol.js`);
 
-//Credentials
-const hueCredentialsLocation = `./credentials/hue.json`;
-const mySQLCredentialsLocation = `./credentials/mysql.json`;
-const discordCredentialsLocation = `./credentials/discord.json`;
-const telegramCredentialsLocation = `./credentials/telegram.json`;
-
-if (fs.existsSync(hueCredentialsLocation)) global.hueCredentials = require(hueCredentialsLocation);
-if (fs.existsSync(mySQLCredentialsLocation)) global.mySQLCredentials = require(mySQLCredentialsLocation);
-if (fs.existsSync(discordCredentialsLocation)) global.discordCredentials = require(discordCredentialsLocation);
-if (fs.existsSync(telegramCredentialsLocation)) global.telegramCredentials = require(telegramCredentialsLocation);
-
 global.commandArray = new Array();
 global.protocolArray = new Array();
 
 global.report = new Report();
 global.emitter = new Events.EventEmitter();
 
-global.getMapperFactory = function() {
-  let MapperFactory = require(`./classes/Mapper/MapperFactory.js`);
-  return new MapperFactory();
-}
-
-global.getRepositoryFactory = function() {
-  let RepositoryFactory = require('./classes/Repository/RepositoryFactory.js');
-  return new RepositoryFactory(getMapperFactory());
-}
+loader.loadCredentials();
+loader.loadFactories();
 
 require('./parsers/telegram.js');
 require('./parsers/discord.js');
---
+++ loader.js
@@ -0,0 +1,43 @@
+"use strict";
+
+let fs = require(`fs`);
+
+module.exports = {
+  loadCredentials: function() {
+    report.log(`Loading credentials`);
+
+    const hueCredentialsLocation = `./credentials/hue.json`;
+    const mySQLCredentialsLocation = `./credentials/mysql.json`;
+    const discordCredentialsLocation = `./credentials/discord.json`;
+    const telegramCredentialsLocation = `./credentials/telegram.json`;
+
+    //Core code credentials
+    if (fs.existsSync(hueCredentialsLocation)) global.hueCredentials = require(hueCredentialsLocation);
+    else report.fatal(`Hue credentials are missing`);
+
+    if (fs.existsSync(mySQLCredentialsLocation)) global.mySQLCredentials = require(mySQLCredentialsLocation);
+    else report.fatal(`MySQL credentials are missing`);
+
+    //Communication platforms credentials
+    if (fs.existsSync(discordCredentialsLocation)) global.discordCredentials = require(discordCredentialsLocation);
+    else report.log(`Discord credentials are missing`);
+
+    if (fs.existsSync(telegramCredentialsLocation)) global.telegramCredentials = require(telegramCredentialsLocation);
+    else report.log(`Telegram credentials are missing`);
+  },
+  loadFactories: function() {
+    report.log(`Loading factories`);
+
+    //Initialize mapper factory
+    global.getMapperFactory = function() {
+      let MapperFactory = require(`./classes/Mapper/MapperFactory.js`);
+      return new MapperFactory();
+    }
+
+    //Initialize repository factory
+    global.getRepositoryFactory = function() {
+      let RepositoryFactory = require('./classes/Repository/RepositoryFactory.js');
+      return new RepositoryFactory(getMapperFactory());
+    }
+  }
+}
