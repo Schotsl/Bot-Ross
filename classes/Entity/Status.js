67266b889f41afd815af933c5591261cf78ffa7b
Sjors van Holst <sjorsvanholst@gmail.com>
2019-07-15 15:35:48 +0200

    Added status object

---
+++ classes/Entity/Status.js
@@ -0,0 +1,43 @@
+"use strict";
+
+module.exports = class Status {
+  constructor() {
+    this.id;
+    this.state;
+    this.person;
+    this.platform;
+  }
+
+  setId(id) {
+    this.id = id;
+  }
+
+  setState(state) {
+    this.status = status;
+  }
+
+  setPerson(person) {
+    this.person = person;
+  }
+
+  setPlatform(platform) {
+    if (platforms.includes(platform)) this.platform = platform;
+    else report.error(`Unsupported platform (${platform}) has been attempted to set`);
+  }
+
+  getId() {
+    return this.id;
+  }
+
+  getState() {
+    return this.state;
+  }
+
+  getPerson() {
+    return this.person;
+  }
+
+  getPlatform() {
+    return this.platform;
+  }
+}
