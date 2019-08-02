e08a76ec3e382d26c14156e80ec02f8e2ce3f21b
Sjors van Holst <sjorsvanholst@gmail.com>
2019-08-02 23:53:22 +0200

    Debugging random crashes

--- parsers/discord.js
+++ parsers/discord.js
@@ -15,38 +15,38 @@ discord.on(`error`, function(data) {
   report.error(data);
 });
 
-discord.on(`message`, async (discordMessageObject) => {
-  //If person is a bot
-  if (discordMessageObject.author.bot) return;
-
-  //Attempt to get user by Discord ID
-  discordIdToPersonObject(discordMessageObject.author.id, (person) => {
-
-    //Create respond function to pass along
-    let respond = function(response) {
-      let messageObject = new Message();
-      messageObject.setPerson(person.id);
-      messageObject.setContent(response);
-      messageObject.setRecieved(0);
-
-      getRepositoryFactory().getMessageRepository().saveMessage(messageObject, function() {
-        //Emit custom message event
-        discordMessageObject.channel.send(messageObject.content);
-      });
-    }
-
-    //Get actual message
-    let messageObject = new Message();
-    messageObject.setPerson(person.id);
-    messageObject.setContent(discordMessageObject.content);
-    messageObject.setRecieved(1);
-
-    getRepositoryFactory().getMessageRepository().saveMessage(messageObject, function() {
-      //Emit custom message event
-      emitter.emit('message', messageObject.content, respond, person)
-    });
-  });
-});
+// discord.on(`message`, async (discordMessageObject) => {
+//   //If person is a bot
+//   if (discordMessageObject.author.bot) return;
+//
+//   //Attempt to get user by Discord ID
+//   discordIdToPersonObject(discordMessageObject.author.id, (person) => {
+//
+//     //Create respond function to pass along
+//     let respond = function(response) {
+//       let messageObject = new Message();
+//       messageObject.setPerson(person.id);
+//       messageObject.setContent(response);
+//       messageObject.setRecieved(0);
+//
+//       getRepositoryFactory().getMessageRepository().saveMessage(messageObject, function() {
+//         //Emit custom message event
+//         discordMessageObject.channel.send(messageObject.content);
+//       });
+//     }
+//
+//     //Get actual message
+//     let messageObject = new Message();
+//     messageObject.setPerson(person.id);
+//     messageObject.setContent(discordMessageObject.content);
+//     messageObject.setRecieved(1);
+//
+//     getRepositoryFactory().getMessageRepository().saveMessage(messageObject, function() {
+//       //Emit custom message event
+//       emitter.emit('message', messageObject.content, respond, person)
+//     });
+//   });
+// });
 
 discord.on("presenceUpdate", function(oldDiscordUserObject, newDiscordUserObject) {
   let oldStatus = oldDiscordUserObject.presence.status;
