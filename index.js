237d59230bfcf2bcf32e0bab391721fff52aacbd
Sjors van Holst <sjorsvanholst@gmail.com>
2019-08-26 16:18:09 +0200

    Added video endpoint

--- index.js
+++ index.js
@@ -117,3 +117,39 @@ app.patch('/emotion', function(request, response) {
   emotionValue = request.body.emotion;
   response.send(JSON.stringify(emotionValue));
 });
+
+//Video endpoint
+let fs = require('fs');
+
+app.get('/birthday/:birthdayId', function(request, response) {
+  let birthdayId = request.params.birthdayId;
+
+  const path = `assets/birthday/${birthdayId}.mp4`
+  const stat = fs.statSync(path)
+  const fileSize = stat.size
+  const range = request.headers.range
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-")
+    const start = parseInt(parts[0], 10)
+    const end = parts[1]
+      ? parseInt(parts[1], 10)
+      : fileSize-1
+    const chunksize = (end-start)+1
+    const file = fs.createReadStream(path, {start, end})
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4',
+    }
+    response.writeHead(206, head);
+    file.pipe(response);
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    }
+    response.writeHead(200, head)
+    fs.createReadStream(path).pipe(response)
+  }
+});
