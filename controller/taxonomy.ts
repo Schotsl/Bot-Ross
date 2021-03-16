18d78fb004118bb8eabf77cf7d3f5a1ef6685102
Sjors van Holst <sjorsvanholst@gmail.com>
2021-03-16 16:23:41 +0100

    ♻️ Refactored Taxonomy endpoint

--- controller/taxonomy.ts
+++ controller/taxonomy.ts
@@ -16,8 +16,15 @@ const addTaxonomy = async (
   const body = await request.body();
   const value = await body.value;
 
-  // Validate string property
-  if (value.title.length === 0) {
+  // Check if required properties have been provided
+  if (!value.title) {
+    response.body = `Missing 'title' property`;
+    response.status = 400;
+    return;
+  }
+
+  // Validate the properties values
+  if (value.title.length < 3 || value.title.length > 255) {
     response.body = `Invalid 'title' property`;
     response.status = 400;
     return;
@@ -25,8 +32,10 @@ const addTaxonomy = async (
 
   // Create new taxonomy and insert
   const taxonomy = new Taxonomy(value.title);
+  const wrapper = await taxonomyDatabase.insertOne(taxonomy);
 
-  taxonomy._id = await taxonomyDatabase.insertOne(taxonomy);
+  // Simplify the ID for the rest API
+  taxonomy.id = wrapper.$oid.toString();
 
   // Return to the user
   response.body = taxonomy;
@@ -66,6 +75,12 @@ const getTaxonomies = async (
   // Get every taxonomy
   const taxonomies = await taxonomyDatabase.find().limit(limit).skip(offset);
   const total = await taxonomyDatabase.count();
+
+  // Simplify the ID for the rest API
+  taxonomies.map((taxonomy) => {
+    taxonomy.id = taxonomy._id!.$oid.toString();
+    taxonomy._id = undefined;
+  })
 
   // Return results to the user
   response.status = 200;
@@ -114,6 +129,10 @@ const updateTaxonomy = async (
 
   // Update taxonomy value
   await taxonomyDatabase.updateOne({ _id: ObjectId(params._id) }, taxonomy);
+
+  // Simplify the ID for the rest API
+  taxonomy.id = taxonomy._id!.$oid.toString();
+  taxonomy._id = undefined;
 
   // Return results to the user
   response.body = taxonomy;
