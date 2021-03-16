d4932707db401de435b4888eb8107cf8f446328d
Sjors van Holst <sjorsvanholst@gmail.com>
2021-03-16 17:18:17 +0100

    🐛 Removed refrence to unused file

--- router.ts
+++ router.ts
@@ -1,12 +1,13 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
 
-import { addMark, deleteMark, getMarks } from "./controller/mark.ts";
-import {
-  addLabel,
-  deleteLabel,
-  getLabels,
-  updateLabel,
-} from "./controller/label.ts";
+// import { addMark, deleteMark, getMarks } from "./controller/mark.ts";
+import { addExpense, deleteExpense, getExpenses, getBreakdown } from "./controller/expense.ts";
+// import {
+//   addLabel,
+//   deleteLabel,
+//   getLabels,
+//   updateLabel,
+// } from "./controller/label.ts";
 import {
   addTaxonomy,
   deleteTaxonomy,
@@ -23,10 +24,10 @@ import {
 const router = new Router();
 
 // Add the label endpoints
-router.get("/label", getLabels);
-router.put("/label/:_id", updateLabel);
-router.post("/label", addLabel);
-router.delete("/label/:_id", deleteLabel);
+// router.get("/label", getLabels);
+// router.put("/label/:_id", updateLabel);
+// router.post("/label", addLabel);
+// router.delete("/label/:_id", deleteLabel);
 
 // Add the contact endpoints
 router.get("/contact", getContacts);
@@ -41,8 +42,14 @@ router.post("/taxonomy", addTaxonomy);
 router.delete("/taxonomy/:_id", deleteTaxonomy);
 
 // Add the label endpoints
-router.get("/mark", getMarks);
-router.post("/mark", addMark);
-router.delete("/mark/:_id", deleteMark);
+// router.get("/mark", getMarks);
+// router.post("/mark", addMark);
+// router.delete("/mark/:_id", deleteMark);
+
+// Add the expense endpoints
+router.get("/expense", getExpenses);
+router.get("/breakdown", getBreakdown);
+router.post("/expense", addExpense);
+router.delete("/expense/:_id", deleteExpense);
 
 export default router;
