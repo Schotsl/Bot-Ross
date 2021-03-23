8f7a33e94108132891850994b80cca9cb0c8be98
Sjors van Holst <sjorsvanholst@gmail.com>
2021-03-23 20:46:00 +0100

    ✨ Added image endpoint

--- controller/image.ts
+++ controller/image.ts
@@ -6,6 +6,7 @@ import { globalDatabase } from "../database.ts";
 import { decode } from "https://deno.land/std@0.63.0/encoding/base64.ts";
 import { ObjectId } from "https://deno.land/x/mongo@v0.13.0/ts/types.ts";
 import { Response } from "https://deno.land/x/oak/mod.ts";
+import * as base64 from "https://denopkg.com/chiefbiiko/base64/mod.ts";
 
 // Create the databases
 const imageDatabase = globalDatabase.collection<Image>("images");
@@ -13,9 +14,8 @@ const imageDatabase = globalDatabase.collection<Image>("images");
 const getImage = async (
   { params, response }: { params: { id: string }; response: Response },
 ) => {
-  console.log('bruh');
   // Get the image using the ID from the URL
-  const image = await imageDatabase.findOne();
+  const image = await imageDatabase.findOne({ _id: ObjectId(params.id) });
 
   // If there is no image found
   if (!image) {
@@ -23,7 +23,16 @@ const getImage = async (
     return;
   }
 
-  
+  // Return the image to the user
+  const semicolonIndex = image.base64!.indexOf(';')
+  const colonIndex = image.base64!.indexOf(':')
+  const commaIndex = image.base64!.indexOf(',')
+
+  const imageSize = image.base64!.slice(colonIndex + 1, semicolonIndex);
+  const imageData = image.base64!.slice(commaIndex + 1);
+
+  response.headers.set('Content-Type', imageSize)
+  response.body = base64.toUint8Array(imageData)
 };
 
 export { getImage };
