a9e0781ce4999976c793ba1ed3be98a523a688c5
Sjors van Holst <sjorsvanholst@gmail.com>
2024-03-27 19:29:57 +0100

    ♻️ Improved cleaning function

--- services/gemini.ts
+++ services/gemini.ts
@@ -20,8 +20,7 @@ class GeminiService {
     const strippedResponse = stripHtml(html);
     const strippedResults = strippedResponse.result.trim();
 
-    const responsePrompt =
-      `You are tasked with cleaning up an email from unnecessary content and random junk, only leave the important parts of the email. The email is as follows:\n\n${strippedResults}`;
+    const responsePrompt = `You are tasked with cleaning up an email from unnecessary content and random junk, only leave the important parts of the email. The email is as follows:\n\n${strippedResults}`;
     const responseObject = await this.model.generateContent([responsePrompt]);
     const responseText = responseObject.response.text();
 
@@ -32,14 +31,16 @@ class GeminiService {
     console.log(`🤖 Verifying email using ${this.name}`);
 
     const requestRules = rules.join(",\n");
-    const requestPrompt =
-      `You are tasked with filtering emails based on their subject and content. You have to return a JSON object with a property called "ignore", this property indicates whether or not the email should be ignored. Don't return anything else except the JSON object! Consider the following rules when determining if a email should be ignored:\n${requestRules}\n\nSubject: ${subject}\n\nBody: ${body}`;
+    const requestPrompt = `You are tasked with filtering emails based on their subject and content. You have to return a JSON object with a property called "ignore", this property indicates whether or not the email should be ignored. Don't return anything else except the JSON object! Consider the following rules when determining if a email should be ignored:\n${requestRules}\n\nSubject: ${subject}\n\nBody: ${body}`;
 
     const responseObject = await this.model.generateContent([requestPrompt]);
     const responseText = responseObject.response.text();
 
     // Turn the response with markdown into a JSON object
-    const jsonPlain = responseText.slice(7, -3);
+    const jsonPlain = responseText.includes("```json")
+      ? responseText.slice(7, -3)
+      : responseText;
+
     const jsonCleaned = jsonPlain.replace(/(\r\n|\n|\r)/gm, "");
     const jsonTrimmed = jsonCleaned.trim();
 
