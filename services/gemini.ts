2f784e3edf611a0358686cc74d8a3611593f35fb
Sjors van Holst <sjorsvanholst@gmail.com>
2024-03-27 18:58:03 +0100

    ♻️ Added parse catch

--- services/gemini.ts
+++ services/gemini.ts
@@ -20,8 +20,7 @@ class GeminiService {
     const strippedResponse = stripHtml(html);
     const strippedResults = strippedResponse.result.trim();
 
-    const responsePrompt =
-      `You are tasked with cleaning up an email from unnecessary content and random junk, only leave the important parts of the email. The email is as follows:\n\n${strippedResults}`;
+    const responsePrompt = `You are tasked with cleaning up an email from unnecessary content and random junk, only leave the important parts of the email. The email is as follows:\n\n${strippedResults}`;
     const responseObject = await this.model.generateContent([responsePrompt]);
     const responseText = responseObject.response.text();
 
@@ -34,17 +33,26 @@ class GeminiService {
     const rulesArray = options.rules;
     const rulesFormatted = rulesArray.join(",\n");
 
-    const responsePrompt =
-      `You are tasked with filtering emails based on their subject and content. You have to return a JSON object with a property called "ignore", this property indicates whether or not the email should be ignored. Consider the following rules when determining if a email should be ignored:\n${rulesFormatted}\n\nSubject: ${subject}\n\nBody: ${body}`;
+    const responsePrompt = `You are tasked with filtering emails based on their subject and content. You have to return a JSON object with a property called "ignore", this property indicates whether or not the email should be ignored. Don't return anything else except the JSON object! Consider the following rules when determining if a email should be ignored:\n${rulesFormatted}\n\nSubject: ${subject}\n\nBody: ${body}`;
     const responseObject = await this.model.generateContent([responsePrompt]);
     const responseText = responseObject.response.text();
 
     // Turn the response with markdown into a JSON object
     const jsonPlain = responseText.slice(7, -3);
     const jsonCleaned = jsonPlain.replace(/(\r\n|\n|\r)/gm, "");
-    const jsonParsed = JSON.parse(jsonCleaned);
+    const jsonTrimmed = jsonCleaned.trim();
 
-    return jsonParsed.ignore;
+    try {
+      const jsonParsed = JSON.parse(jsonTrimmed);
+      const jsonIgnore = jsonParsed.ignore;
+
+      return jsonIgnore;
+    } catch (error) {
+      console.error("🚨 Error parsing JSON response from Gemini");
+      console.error(jsonTrimmed);
+
+      return false;
+    }
   }
 }
 
