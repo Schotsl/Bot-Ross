00c0bbe3c2b87cf5db75c3ad6371700914411191
Sjors van Holst <sjorsvanholst@gmail.com>
2024-03-27 17:15:20 +0100

    🎨 Formatted codebase

--- services/gemini.ts
+++ services/gemini.ts
@@ -20,7 +20,8 @@ class GeminiService {
     const strippedResponse = stripHtml(html);
     const strippedResults = strippedResponse.result.trim();
 
-    const responsePrompt = `You are tasked with cleaning up an email from unnecessary content and random junk, only leave the important parts of the email. The email is as follows:\n\n${strippedResults}`;
+    const responsePrompt =
+      `You are tasked with cleaning up an email from unnecessary content and random junk, only leave the important parts of the email. The email is as follows:\n\n${strippedResults}`;
     const responseObject = await this.model.generateContent([responsePrompt]);
     const responseText = responseObject.response.text();
 
@@ -33,7 +34,8 @@ class GeminiService {
     const rulesArray = options.rules;
     const rulesFormatted = rulesArray.join(",\n");
 
-    const responsePrompt = `You are tasked with filtering emails based on their subject and content. You have to return a JSON object with a property called "ignore", this property indicates whether or not the email should be ignored. Consider the following rules when determining if a email should be ignored:\n${rulesFormatted}\n\nSubject: ${subject}\n\nBody: ${body}`;
+    const responsePrompt =
+      `You are tasked with filtering emails based on their subject and content. You have to return a JSON object with a property called "ignore", this property indicates whether or not the email should be ignored. Consider the following rules when determining if a email should be ignored:\n${rulesFormatted}\n\nSubject: ${subject}\n\nBody: ${body}`;
     const responseObject = await this.model.generateContent([responsePrompt]);
     const responseText = responseObject.response.text();
 
