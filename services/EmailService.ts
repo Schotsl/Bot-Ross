b771d4fd679f1ecd0c99541960bfa9a91b71ec2d
Sjors van Holst <sjorsvanholst@gmail.com>
2024-04-06 21:21:49 +0200

    ⏪️ Reverted change

--- services/EmailService.ts
+++ services/EmailService.ts
@@ -106,6 +106,9 @@ class EmailService {
       useLabels: true,
       uid: true,
     });
+
+    // Move the email to the "Ignore" mailbox
+    await this.client!.messageMove({ uid }, "Ignore", { uid: true });
   }
 
   async fetchEmails(filter = ["Scanned"]) {
